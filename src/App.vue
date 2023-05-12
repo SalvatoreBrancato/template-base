3fa7ddcaff2ca1e1099e2cdd44b2c4b6b26d50f3
SalvatoreBrancato <salvatorebrancato90@gmail.com>
2023-05-12 13:49:33 +0200

    Template + comp + store

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,24 @@
 <script>
+import MainComp from './components/MainComp.vue';
+import {store} from './store'
+
+
+
 export default{
   name: "App",
   components:{
-    
+    MainComp
+},
+data(){
+  return{
+    store
   }
+}
 }
 </script>
 
 <template>
-
+  <MainComp/>
 </template>
 
 <style lang="scss">
